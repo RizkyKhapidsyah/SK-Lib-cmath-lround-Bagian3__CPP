d501f07df232ec21e378b50be45db0c9814b8e4a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-21 19:48:57 +0700

    Add project files.

---
+++ SK-Lib-cmath-lround-Bagian3__CPP/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int x = 67;
+    long int hasil;
+    hasil = lround(x);
+    cout << "lround(" << x << ") = " << hasil << endl;
+    _getch();
+    return 0;
+}
